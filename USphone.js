8b73d50ce494e2da37240b23552ed605232aaa1a
Vladimir Chavez <vladimirsound@outlook.com>
2018-10-04 17:40:51 -0500

    On branch dev  Changes to be committed: 	modified:   USphone.js
    	--Program complete and submitted to FCC.
    	--Can clean up by using class to store functions if desired.

--- USphone.js
+++ USphone.js
@@ -1,92 +1,116 @@
-/**
- * CODES:
- * 0-9 = [48,57]
- * () = 40,41 | valid at index {(0,4)}
- * "_" = 32 | valid at {(1,5,9),(3,7),5}
- * - = 45 | valid @ {(3,7),8,9}
- *
- * VALID:
- * 555-555-5555 -(3,7) | NUMBERS
- (555)555-5555 P(0,4) * -(8) * NUMBERS{1,%,12}
- (555) 555-5555 P(0,4) * s(5) * -(9) * NUM{1,%,13}
- 555 555 5555 S(3,7) * NUM{0,%,11}
- 5555555555 NUM{0,9}
- 1 555 555 5555 S(1,5,9) * NUM{0,%,13}
- */
-
-
 
 main();
 
 function main() {
-	console.log(telephoneCheck("1 555 555 5555"));
+	let testCases = [
+		["555-555-5555","true"],
+		["(555)555-5555","true"],
+		["(555) 555-5555","true"],
+		["555 555 5555","true"],
+		["5555555555","true"],
+		["1 555 555 5555","true"],
+		["1 555)555-5555","false"],
+		["555)555-5555","false"]
+	];
+
+	testCases.forEach(function(value){
+		console.log(value);
+		console.log("\n" + telephoneCheck(value[0]) + "\n\n");
+	});
 }
 
-function telephoneCheck(str){
-	console.log("checking number: " + str);
-	//LOGICAL CASE VARIABLES
-	//let a = isSpace(str, 3) && isSpace(str, 7);
-	let b = isP(str, 0) && isP(str, 4);
-	let c = isP(str, 8);
-	let d = isSpace(str, 5);
-	let e = isP(str, 9);
-	/* let f = isSpace(str, 3) && isSpace(str, 7);
-	let g = areDigits(str,0,9);
-	let h = isSpace(str,1) && isSpace(str,5) && isSpace(str, 9); */
+/**
+ * Checks to see if a string is a valid US Phone Number.
+ * @author Johnny Chavez
+ * @param {String} phone 
+ */
+function telephoneCheck(phone){
+	console.log("checking number: " + phone);
+
+	//Get rid of US international code +1
+	let str = checkForOne(phone);
 	
-	
-	if(((isDash(str,3) && isDash(str,7)) || (isSpace(str,3) && isSpace(str,7)))
-		&& remainingDigits(str, [3,7],11)) {
-		console.log("check 1: ");
+	//Cases when the phone number contains '-' or '_'.
+	if(((isDash(str,3) && isDash(str,7)) || (isSpace(str,3) && isSpace(str,7))) && remainingDigits(str, [3,7],11)) 
 		return true;
-	}
-	
+	//Cases for when the phone number contains '-' or '_' or '(',')'
 	if(((isDash(str, 8) && remainingDigits(str,[0,4,8],12)) || (isSpace(str,5) && isDash(str, 9) && remainingDigits(str, [0,4,5,9], 13))) && isP(str,0) && isP(str,4))
 		return true;
-	
-	if(areDigits(str,0,9))
+
+	//Cases for when the phone number is only digits with no International Code.
+	if(areDigits(str) && str.length === 10)
 		return true;
 	
 	if(isSpace(str, 1) && isSpace(str, 5) && isSpace(str, 9) && remainingDigits(str,[1,5,9],13))
 		return true;
-	
+
 	return false;
 }
 
+/**
+ * Checks if a character is either an opening or closing parenthesis.
+ * @param {String} str
+ * @param {Number} pos
+ * @returns {boolean} 
+ */
 function isP(str, pos) {
 	return str.charCodeAt(pos) === 40 || str.charCodeAt(pos) === 41;
 }
 
+/**
+ * Checks if a character is a dash '-'.
+ * @param {String} str 
+ * @param {number} pos 
+ * @returns {boolean} true if the character is a '-'.
+ */
 function isDash(str, pos) {
 	return str.charCodeAt(pos) === 45;
 }
 
+/**
+ * Checks if a character is an empty space.
+ * @param {String} str 
+ * @param {number} pos 
+ */
 function isSpace(str, pos) {
 	return str.charCodeAt(pos) === 32;
 }
 
+/**
+ * Checks if the character is a digit. {0,...,9}
+ * @param {String} str 
+ * @param {number} pos 
+ */
 function isDigit(str, pos) {
 	let code = str.charCodeAt(pos);
 	return code >= 48 && code <= 57
 }
 
-function areDigits(str, x, y) {
-	let result = true;
-	for(x; x <=y; x++){
-		if(!isDigit(str, x))
-			result = false;
-	}
-	return result;
+/**
+ * Checks if there is some character in str that is not a digit.
+ * @param {String} str 
+ */
+function areDigits(str) {
+	let arr = str.split("");
+	return !arr.some(function(element){
+		return !isDigit(element,0);
+	});
 }
 
 /**
- *
- * @param str The String to be checked.
- * @param chkd  An int[] of indexes that have been checked.
- * @param max The max length for this case.
- * @return true if all unchecked indexes within the range are digits.
+ * Checks to see if all characters in str that are not in chkd[] are digits.
+ * @param {String} str The String to be checked.
+ * @param {number[]} chkd  An int[] of indexes that have been checked.
+ * @param {number} max The max length for this case.
+ * @return {boolean} true if all unchecked indexes within the range are digits.
  */
 function remainingDigits(str, chkd, max){
+	//check that max is the actual max of the string.
+	if(str.length !== max + 1){
+		console.log("str length: " + str.length + "\nmax: " + max);
+		return false;
+	}
+
 	//Get unchecked indexes of the string.
 	let unChkd = [];
 	for(let x = 0; x <= max; x++){
@@ -100,7 +124,13 @@ function remainingDigits(str, chkd, max){
 		if(!isDigit(str,unChkd[x]))
 			result = false;
 	}
-	console.log("c: " + chkd);
-	console.log("unch: " + unChkd);
+	
 	return result;
+}
+
+function checkForOne(str){
+	let code = str.charCodeAt(0);
+	if(code === 49 || isSpace(str,0))
+		return checkForOne(str.substring(1));
+	return str;
 }
